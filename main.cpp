dfb9d8732f35a6a3c0112f29d4d65fe18f621ace
Kristian Schlikow <dakraid@digital-dragons.de>
2019-11-06 03:26:02 +0100

    First prototype implementation success
    Currently causes CTDs on second inventory access. Marker isn't being set. Plugin uses base values instead of "actual" values, should be an option.

--- main.cpp
+++ main.cpp
@@ -13,34 +13,34 @@
 #include <SKSE/SafeWrite.h>
 #include <SKSE/Version.h>
 
-#include "date.h"
-
 #include <algorithm>
 #include <vector>
 
-static const char *g_pluginName = "BestInClass++";
-const UInt32 g_pluginVersion = 100;
+#include "date.h"
+
+static const char* g_pluginName = "BestInClass++";
+const UInt32 g_pluginVersion = 108;
 
 class Plugin_BestInClassPP_plugin : public SKSEPlugin,
                                     public BSTEventSink<MenuOpenCloseEvent> {
 private:
-  /* bestItem Structure
+    /* bestItem Structure
   The bestItem struct is part of an attempt at optimizing the code.
   Instead of accessing the stored item to get its value for comparison
   we save it separately with it.
   */
-  struct bestItem {
-    StandardItemData *itemData;
-    float comparisonValue;
-  };
+    struct bestItem {
+        StandardItemData* itemData;
+        float comparisonValue;
+    };
 
-  /* bestItem Index Assignment
+    /* bestItem Index Assignment
   The bestItemArray assigns an item type to an index
 
   Armors
   0	LightArmor		| 5	HeavyArmor
   1	LightBoots		| 6	HeavyBoots
-  2	LightGauntlets| 7	HeavyGauntlets
+  2	LightGauntlets	| 7	HeavyGauntlets
   3	LightHelmet		| 8	HeavyHelmet
   4	LightShield		| 9	HeavyShield
 
@@ -50,85 +50,110 @@ private:
   12	1HMace			| 16	2HWarhammer
   13	1HDagger
   */
-  bestItem bestItemArray[16];
+    bestItem bestItemArray[16];
 
-  void LogMessage(const char *fmt, ...) {
-    va_list args;
-    va_start(args, fmt);
-    char formatBuf[1024];
-    vsprintf_s(formatBuf, fmt, args);
-    va_end(args);
+    void LogMessage(const char* fmt, ...)
+    {
+        va_list args;
+        va_start(args, fmt);
+        char inputBuf[1024];
+        vsprintf_s(inputBuf, fmt, args);
+        va_end(args);
 
-    std::string currTime =
-        date::format("%F %T", std::chrono::system_clock::now());
+        std::string timestamp = date::format("%F %T", std::chrono::system_clock::now());
+        std::string message = inputBuf;
+        std::string output = "[" + timestamp + "] " + message;
 
-    _MESSAGE("[%s] %s", currTime, formatBuf);
-  }
+        _MESSAGE("%s", output);
+    }
 
 public:
-  Plugin_BestInClassPP_plugin() {}
+    Plugin_BestInClassPP_plugin() { }
 
-  virtual bool InitInstance() override {
-    if (!Requires(kSKSEVersion_1_7_1, SKSEPapyrusInterface::Version_1)) {
-      gLog << "ERROR: Your SKSE Version is too old." << std::endl;
-      return false;
-    }
-
-    SetName(g_pluginName);
-    SetVersion(g_pluginVersion);
-
-    return true;
-  }
-
-  virtual bool OnLoad() override {
-    SKSEPlugin::OnLoad();
-
-    MenuManager *mm = MenuManager::GetSingleton();
-    mm->BSTEventSource<MenuOpenCloseEvent>::AddEventSink(this);
-
-    // TESDataHandler *dh = DataHandler::GetSingleton();
-
-    return true;
-  }
-
-  virtual EventResult
-  ReceiveEvent(MenuOpenCloseEvent *evn,
-               BSTEventSource<MenuOpenCloseEvent> *src) override {
-    UIStringHolder *holder = UIStringHolder::GetSingleton();
-    if (evn->menuName != holder->console)
-      return kEvent_Continue;
-
-    if (evn->opening) {
-      LogMessage("Menu \"%s\" has been opened", evn->menuName);
-
-      MenuManager *mm = MenuManager::GetSingleton();
-      IMenu *menu = mm->GetMenu(holder->inventoryMenu);
-
-      if (menu) {
-        InventoryMenu *invMenu = static_cast<InventoryMenu *>(menu);
-        BSTArray<StandardItemData *> &itemDataArray =
-            invMenu->inventoryData->items;
-
-        LogMessage("Found %d items within the inventory array",
-                   itemDataArray.GetSize());
-
-        if (itemDataArray.GetSize() > 0) {
-          for (StandardItemData *itemData : itemDataArray) {
-            LogMessage("Current item: %s", itemData->GetName());
-
-            if (itemData->objDesc) {
-              TESForm *temp = itemData->objDesc->baseForm;
-              if (temp->IsWeapon() || temp->IsArmor()) {
-                LogMessage("Current type: %s", temp->GetTypeString());
-              }
-            }
-          }
+    virtual bool InitInstance() override
+    {
+        if (!Requires(kSKSEVersion_1_7_1, SKSEPapyrusInterface::Version_1)) {
+            gLog << "ERROR: Your SKSE Version is too old." << std::endl;
+            return false;
         }
-      }
+
+        SetName(g_pluginName);
+        SetVersion(g_pluginVersion);
+
+        return true;
     }
 
-    return kEvent_Continue;
-  }
+    virtual bool OnLoad() override
+    {
+        SKSEPlugin::OnLoad();
 
-  virtual void OnModLoaded() override {}
+        MenuManager* mm = MenuManager::GetSingleton();
+        mm->BSTEventSource<MenuOpenCloseEvent>::AddEventSink(this);
+
+        return true;
+    }
+
+    virtual EventResult ReceiveEvent(
+        MenuOpenCloseEvent* evn,
+        BSTEventSource<MenuOpenCloseEvent>* src) override
+    {
+        UIStringHolder* holder = UIStringHolder::GetSingleton();
+        if (evn->opening && (evn->menuName == holder->inventoryMenu || evn->menuName == holder->barterMenu || evn->menuName == holder->containerMenu)) {
+            _MESSAGE("Menu \"%s\" has been opened", evn->menuName);
+
+            MenuManager* mm = MenuManager::GetSingleton();
+            IMenu* menu = mm->GetMenu(holder->inventoryMenu);
+
+            if (menu) {
+                InventoryMenu* invMenu = static_cast<InventoryMenu*>(menu);
+                BSTArray<StandardItemData*>& itemDataArray = invMenu->inventoryData->items;
+
+                if (!itemDataArray.empty()) {
+                    for (StandardItemData* itemData : itemDataArray) {
+                        _MESSAGE("Current Item: %s", itemData->GetName());
+                        TESForm* baseForm = itemData->objDesc->baseForm;
+                        if (baseForm->IsWeapon()) {
+                            _MESSAGE("Current Weapon: %s", itemData->GetName());
+                            TESObjectWEAP* objWEAP = DYNAMIC_CAST<TESObjectWEAP*>(baseForm);
+
+                            if (objWEAP) {
+                                switch (objWEAP->gameData.type) {
+                                case 1:
+                                    if (bestItemArray[10].itemData) {
+                                        if (objWEAP->attackDamage > bestItemArray[10].comparisonValue) {
+                                            bestItemArray[10].itemData = itemData;
+                                            bestItemArray[10].comparisonValue = objWEAP->attackDamage;
+                                        }
+                                    } else {
+                                        bestItemArray[10].itemData = itemData;
+                                        bestItemArray[10].comparisonValue = objWEAP->attackDamage;
+                                    }
+                                    break;
+                                default:
+                                    break;
+                                }
+                            }
+                        } else if (baseForm->IsArmor()) {
+                            _MESSAGE("Current Armor: %s", itemData->GetName());
+                            // TODO
+                        }
+                    }
+                }
+            }
+        } else {
+            return kEvent_Continue;
+        }
+
+        if (bestItemArray[10].itemData) {
+            _MESSAGE("The best 1H Sword is %s",
+                bestItemArray[10].itemData->GetName());
+        }
+
+        return kEvent_Continue;
+    }
+
+    virtual void
+    OnModLoaded() override
+    {
+    }
 } thePlugin;
